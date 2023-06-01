9f4c636b1e1c60fa84e943b89c6652ace5081a79
solrak <luiscarlosquesada@gmail.com>
2023-05-31 23:38:26 -0600

    Initial Commit

---
+++ SwiftNotes/SwiftNotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftNotesApp.swift
+//  SwiftNotes
+//
+//  Created by Luis Carlos Quesada Rodriguez on 31/5/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftNotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
